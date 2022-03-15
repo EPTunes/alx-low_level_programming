f361f395fb00bd977d33f64ff20df1929156a190
EPTunes <ekwuribennaemeka@gmail.com>
2022-03-15 13:15:44 -0700

    printing _putchar and a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - prints _putchar, followed by a new line
+ * Return: ALways 0 (Success)
+ */
+int main (void)
+
+{
+	_putchar('_');
+	_Putchar('p');
+	_Putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchat('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
